5a534c2c8fa3e80fd58b20fdf593c7a902127b50
Kevin Huang <yellow.khaos@gmail.com>
2019-02-03 17:46:39 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import React, {Component} from 'react';
 export default class App extends Component {
   constructor () {
     super()
+    this.state={
+      peopleinSpace: []
+    }
   }
   componentDidMount () {
     fetch('http://api.open-notify.org/astros.json')
+    .then(response=>response.json())
+    .then()
   }
   render() {
     return (
